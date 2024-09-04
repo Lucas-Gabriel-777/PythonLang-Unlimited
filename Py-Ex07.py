06647e1e6c91080eca725bad8dd7111bf991f483
Lucas-Gabriel-777 <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-04 14:57:02 -0300

    Update Py-Ex07.py

--- Py-Ex07.py
+++ Py-Ex07.py
@@ -2,7 +2,11 @@
 #Data.: 04-05-2024
 #Objetivo: Criar um programa que leia as duas notas de um aluno e calcule a média
 
-n1= float(input('Qual foi a primeira nota do aluno? '))
+#Definição de variáveis   n1 = Primeiro Número   n2 = Segundo Número    m = Média
+
+n1= float(input('Qual foi a primeira nota do aluno? '))  # Recebe informações pelo teclado
 n2 = float(input('E qual foi a segunda nota? '))
-m = (n1+n2) / 2
-print('A média das notas do(a) aluno(a) é {}'.format(m))
+
+m = (n1+n2) / 2    #Cálculo da média
+
+print('A média das notas do(a) aluno(a) é {}'.format(m))  #Exibe informações na tela
