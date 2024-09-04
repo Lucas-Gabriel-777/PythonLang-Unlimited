267f5523a8da9ce00e62ef4f7e9790ebe065e5f7
Lucas-Gabriel-777 <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-04 14:55:58 -0300

    Update Py-Ex06.py

--- Py-Ex06.py
+++ Py-Ex06.py
@@ -1,12 +1,18 @@
 #Autor: Lucas Gabriel
 #Data.: 27-04-2024
-#Objetivo:Criar um programa que leia um número e mostre o dobro, o triplo e a raiz
-# quadrada desse número
+#Objetivo:Criar um programa que leia um número e mostre o dobro, o triplo e a raiz quadrada desse número
 
+#Definição de variáveis     num = Número    d = Dobro   t = Triplo     rq = Raiz quadrada
+
+#Recebe um número pelo teclado
 num = int(input('Me dê um número: '))
+
+#Cálculo de valores das variáveis
 d = num * 2
 t = num * 3
 rq = num ** (1/2)
+
+#Exibe os resultados na tela
 print('O dobro de {} é {}'.format(num,d))
 print('O triplo é {}'.format(t))
 print('E a raiz quadrada é {}'.format(rq))
