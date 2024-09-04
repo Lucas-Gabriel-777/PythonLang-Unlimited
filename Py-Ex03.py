29c53ccb2e9d11e7315f8514152d69dbcd0a2656
Lucas-Gabriel-777 <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-04 14:52:02 -0300

    Update Py-Ex03.py

--- Py-Ex03.py
+++ Py-Ex03.py
@@ -2,7 +2,14 @@
 #Data.: 27-04-2024
 #Objetivo: Crie um programa que leia dois números e mostre a soma entre eles
 
+# Definição de variáveis     n1 = Primeiro Número    n2 = Segundo Número    SOMA = Soma dos dois números
+
 n1 = int(input('Me dê um número: '))
 n2 = int(input('Agora outro: '))
+
+#Cálculo da soma
 SOMA = n1+n2
+
+#Exibe os valores na tela
 print('A soma entre {} e {} é igual a {}.'.format(n1,n2,SOMA))
+#✟
