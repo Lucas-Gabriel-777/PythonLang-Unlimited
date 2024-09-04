565f76e4157a1985b13679124d074ca72ef359e4
Lucas-Gabriel-777 <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-04 14:36:21 -0300

    Update Py-Ex09.py

--- Py-Ex09.py
+++ Py-Ex09.py
@@ -1,11 +1,15 @@
 #Autor: Lucas gabriel dos Santos Lima
 #Data: 05-05-2024
-#Objetivo: Criar um programa que leia um número pelo teclado e mostre
-# a sua tabuada
+#Objetivo: Criar um programa que leia um número pelo teclado e mostre a sua tabuada
 
-n = int(input('Número para cálculo de tabuada: '))
-c = m = 1
-while c <= 10:
-    m = n * c
-    print('{} X {} = {}'.format(n,c,m))
-    c+=1
+#Definição de variáveis   n = Número   c = Contador    m = Multiplicador
+
+n = int(input('Número para cálculo de tabuada: ')) # Recebe valor pelo teclado
+
+c = m = 1    #Variáveis são iniciadas com o valor "1"
+
+while c <= 10:  #Enquanto o contador for menor que 10, faça:
+
+    m = n * c  #Cálculo da tabuada
+    print('{} X {} = {}'.format(n,c,m))  #Exibe a a linha da tabuada de acordo com o contador
+    c+=1   #Incremento: Contador recebe contador + 1
