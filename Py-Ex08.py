bd53b292d372d1d1bf8e2004ade8983c9ad7f7d3
Lucas-Gabriel-777 <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-04 14:57:30 -0300

    Update Py-Ex08.py

--- Py-Ex08.py
+++ Py-Ex08.py
@@ -1,10 +1,12 @@
 #Autor: Lucas gabriel dos Santos Lima
 #Data: 05-05-2024
-#Objetivo: Criar um programa que leia uma medida em metros e a converta
-# para centímetros e milímetros
+#Objetivo: Criar um programa que leia uma medida em metros e a converta  para centímetros e milímetros
 
-m = float(input('Medida em metros a ser convertida: '))
-c = m * 100
-mi = m * 1000
+#Definição de variáveis    m = Medida  c = Centímetros     mi = Milímetros
 
-print('{} Metros equivalem a {} centímetros e {} milímetros'.format(m,c,mi))
+m = float(input('Medida em metros a ser convertida: '))   #Recebe informações pelo teclado
+
+c = m * 100    #Conversão de medidas para centímetros
+mi = m * 1000   #Conversão de medidas para milímetros
+
+print('{} Metros equivalem a {} centímetros e {} milímetros'.format(m,c,mi))    #Exibe informações na tela
