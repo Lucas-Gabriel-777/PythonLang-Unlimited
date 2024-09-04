6acf657b2054a689d59d7ad25e821bda856b17d5
Lucas-Gabriel-777 <167631579+Lucas-Gabriel-777@users.noreply.github.com>
2024-09-04 14:42:36 -0300

    Update Py-Ex02.py

--- Py-Ex02.py
+++ Py-Ex02.py
@@ -1,5 +1,8 @@
 #Autor: Lucas Gabriel
 #Data.: 27-04-2024
 #Objetivo: Cie um programa que leia o nome do usuário e mostre uma mensagem de boas vindas
+
+# Definição de variáveis  nome = Nome do usuário
+
 nome = input('Digite seu nome: ')
-print('Seja bem vindo(a) {}!'.format(nome))
+print('Seja bem vindo(a) {}!'.format(nome))   #{} : Máscara de formatação
